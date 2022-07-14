d0cf663ff3fd1368e9725370318cc7d26bcbbcdf
Cooper Bell <john.cooper.bell@gmail.com>
2022-07-14 11:17:42 -0500

    Add hammerspoon

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,67 @@
+-- Show terminal
+hs.hotkey.bind({"alt"}, "space", function()
+    terminal = hs.application.get("Alacritty")
+    if terminal:isFrontmost() then
+      terminal:hide()
+    else
+      terminal:activate()
+    end
+end)
+
+hs.hotkey.bind({"ctrl", "alt"}, "space", function()
+    emacs = hs.application.get("Emacs")
+    if emacs:isFrontmost() then
+      emacs:hide()
+    else
+      emacs:activate()
+    end
+end)
+
+-- lock screen
+hs.hotkey.bind({"cmd", "shift"}, "O", function()
+  hs.caffeinate.lockScreen()
+end)
+
+hs.hotkey.bind({"cmd"}, "home", function()
+  hs.spotify.playpause()
+end)
+
+hs.hotkey.bind({"cmd"}, "pagedown", function()
+  playing = hs.spotify.isPlaying()
+
+  if playing then
+    hs.spotify.volumeDown()
+  else
+    output = hs.audiodevice.defaultOutputDevice()
+    output:setVolume(output:volume() - 10)
+  end
+end)
+
+hs.hotkey.bind({"cmd"}, "pageup", function()
+  playing = hs.spotify.isPlaying()
+
+  if playing then
+    hs.spotify.volumeUp()
+  else
+    output = hs.audiodevice.defaultOutputDevice()
+    output:setVolume(output:volume() + 10)
+  end
+end)
+
+caffeine = hs.menubar.new()
+function setCaffeineDisplay(state)
+    if state then
+        caffeine:setTitle("AWAKE")
+    else
+        caffeine:setTitle("SLEEPY")
+    end
+end
+
+function caffeineClicked()
+    setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+
+if caffeine then
+    caffeine:setClickCallback(caffeineClicked)
+    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end
